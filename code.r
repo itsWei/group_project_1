7fcff19d145b0b31b15731e19cda3ed1cb277ae5
Corarara <114095284+Corarara@users.noreply.github.com>
2022-10-04 13:12:12 +0100

    Update code.r
    add comment

--- code.r
+++ code.r
@@ -6,8 +6,8 @@ a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers
 
 
 ###########################
+#4)
 #separate the punctuation marks ","
-
 split_punct <- function (x,a) {
   punct_loc <- grep(x, a, fixed=TRUE)
   delet_list <- gsub(x,"", a, fixed=TRUE)
@@ -17,78 +17,104 @@ split_punct <- function (x,a) {
   empty_vec[-i]<-delet_list
   empty_vec
 }
-a1<-split_punct(",",a)
+
+#5)
+#separate the punctuation marks ","
+a1 <- split_punct(",",a)
 
 #separate the punctuation marks "."
-a2<-split_punct(".",a1)
+a2 <- split_punct(".",a1)
 
 #separate the punctuation marks ";"
-a3<-split_punct(";",a2)
+a3 <- split_punct(";",a2)
 
 #separate the punctuation marks ":"
-a4<-split_punct(":",a3)
+a4 <- split_punct(":",a3)
 
 #separate the punctuation marks "!"
-a5<-split_punct("!",a4)
+a5 <- split_punct("!",a4)
 
 #separate the punctuation marks "?"
-a6<-empty_vec <- split_punct("?",a5)
+a6 <- split_punct("?",a5)
 
 #6(a)
-lower_a<-tolower(a6)
+#find the vector of unique words
+lower_a<-tolower(a6) #replace capital letters in words with lower case letters
 b<-unique(lower_a)
+
 #6(b)
-match(lower_a,b)
+#find Bible text indicies in the unique word vector b
+index_a_in_b <- match(lower_a,b)
+
 #6(c)
-tabulate(match(lower_a,b))
+#find the vector of frequency of unique words from b in Bible text
+unique_frequence <- tabulate(index_a_in_b)
+
 #6(d)
-unique_order<-order(-tabulate(match(lower_a,b)))
+#find the 500 most common words
+unique_order<-order(-unique_frequence) #sort the unique_frequence in decresing order
 m<-500
-unique_order_m<-unique_order[1:m]
+unique_order_m<-unique_order[1:m] # vector of indicies of the 500 most common words
+
 #6(e)
+#create the 500 most commonly occurring words
 b<-b[unique_order_m]
+
 #7(a)
+#find Bible text indicies in the 500 most common words b
 d<-match(lower_a,b)
+
 #7(b)
-matrix<-cbind(d[1:(length(lower_a)-2)],d[1:(length(lower_a)-1)][-1],d[-1][-1])
+#create a matrix with the first column of the index of common words, second column of the index of following word, and the third coloumn of the index of the following word shift one place
+len_lower_a <- length(lower_a) # length of lower_a
+matrix<-cbind(d[1:(len_lower_a-2)],d[1:(len_lower_a-1)][-1],d[-1][-1]) #remove the last two rows
+
 #7(c)
-sum_matrix<-rowSums(is.na(matrix))
-nona_row_index<-which(sum_matrix==0)
-matrix<-matrix[nona_row_index,]
+#drop word triplets that contain an NA
+sum_matrix<-rowSums(is.na(matrix)) # identify NA
+nona_row_index<-which(sum_matrix==0) # find the index of row without NA
+matrix<-matrix[nona_row_index,] # set the matrix by droping the row with NA
+
 #7(d)
-matrix_t<-array(0,dim=c(m,m,m))
-matrix_t <- array(0,dim = c(m,m,m))
+#create maritx T 
+T<-array(0,dim=c(m,m,m)) # (initialize T) create a zero matrix with dimension of m*m*m
 
+#adding a 1 to T[i,k,j] every time the jth common word follows the pair i,k
 for (i in (1:dim(matrix)[1])){
-  matrix_t[matrix[i,1],matrix[i,2],matrix[i,3]] <- matrix_t[matrix[i,1],matrix[i,2],matrix[i,3]] +1
+  T[matrix[i,1],matrix[i,2],matrix[i,3]] <- T[matrix[i,1],matrix[i,2],matrix[i,3]] +1
 }
+
 #7(e)
+
 #7(f)
+#create a matrix A with the first column of the index of common words, second column of the index of following word
 matrix2<-cbind(d[1:(length(lower_a)-1)],d[-1])
-sum_matrix2<-rowSums(is.na(matrix2))
-nona_row_index2<-which(sum_matrix2==0)
-matrix2<-matrix2[nona_row_index2,]
-matrix_a<-array(0,dim=c(m,m))
-matrix_a <- array(0,dim = c(m,m))
+sum_matrix2<-rowSums(is.na(matrix2)) # identify NA
+nona_row_index2<-which(sum_matrix2==0) # find the index of row without NA
+matrix2<-matrix2[nona_row_index2,] # set the matrix2 by droping the row with NA
+A <- array(0,dim = c(m,m)) # (initialize A) create a zero matrix with dimension of m*m
 
+#adding a 1 to A[i,j] every time the jth common word follow i
 for (i in (1:dim(matrix2)[1])){
-  matrix_a[matrix2[i,1],matrix2[i,2]] <- matrix_a[matrix2[i,1],matrix2[i,2]]+1
+  A[matrix2[i,1],matrix2[i,2]] <- A[matrix2[i,1],matrix2[i,2]]+1
 }
-#####
+
+#create a matrix S with the index of common words
 matrix3<-d
-nona_row_index3<-which(is.na(matrix3)==FALSE)
-matrix3<-matrix3[nona_row_index3]
-matrix_s<-rep(0,m)
+nona_row_index3<-which(is.na(matrix3)==FALSE) # identify NA and find the index of row without NA
+matrix3<-matrix3[nona_row_index3] # set the matrix3 by droping the row with NA
+S<-rep(0,m) # (initialize S) create a zero matrix with dimension of m
 
-
+#adding a 1 to S[i] every time the ith common word is never followed by a word in b
 for (i in (1:length(matrix3))){
-  matrix_s[matrix3[i]] <- matrix_s[matrix3[i]]+1
+  S[matrix3[i]] <- S[matrix3[i]]+1
 }
 
-first_word_index <- sample(m,1,prob=matrix_s)
+#8
+first_word_index <- sample(m,1,prob=S)
 #first_word <- b[first_word_index]
 if (sum(matrix_a[first_word_index,]) == 0){
-  second_word_index <- sample(m,1,prob=matrix_s)
+  second_word_index <- sample(m,1,prob=S)
 }else{
   second_word_index <- sample(m,1,prob=matrix_a[first_word_index,])
 } 
@@ -100,12 +126,12 @@ pairs_index[2] <- second_word_index
 for (i in (3:50)){
   if (sum(matrix_t[pairs_index[i-2],pairs_index[i-1],]) == 0){
     if (sum(matrix_a[pairs_index[i-2],]) == 0){
-      pairs_index[i] <- sample(m,1,prob=matrix_s)
+      pairs_index[i] <- sample(m,1,prob=S)
     }else{
-      pairs_index[i] <- sample(m,1,prob=matrix_a[pairs_index[i-2],])
+      pairs_index[i] <- sample(m,1,prob=A[pairs_index[i-2],])
     }
   }else{
-    pairs_index[i] <- sample(m,1,prob=matrix_t[pairs_index[i-2],pairs_index[i-1],])
+    pairs_index[i] <- sample(m,1,prob=T[pairs_index[i-2],pairs_index[i-1],])
   }
 }  
 
@@ -115,6 +141,6 @@ words <- cat(b[pairs_index])
 
 new_pairs_index <- rep(0,50)
 for (i in (1:50)){
-  new_pairs_index[i] <- sample(m,1,prob=matrix_s)
+  new_pairs_index[i] <- sample(m,1,prob=S)
 }
 words_from_s <- cat(b[new_pairs_index])
